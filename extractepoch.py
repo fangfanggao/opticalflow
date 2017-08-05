1642a4e677a4b7b24112f2fa4841539cfa85bfff
Sujay Yadawadkar <sujayr91@vt.edu>
2017-08-04 20:13:22 -0400

    addedclipping

--- extractepoch.py
+++ extractepoch.py
@@ -10,5 +10,17 @@ for folder in folders:
 	tripvideos= os.listdir(videopath)
 	for tripvideo in tripvideos:
 		video= os.path.join(videopath, tripvideo)
-		print(video)	
-	
+                vidcap = cv2.VideoCapture(video)
+                starttime=20000
+                endtime=40000
+                vidcap.set(cv2.CAP_PROP_POS_MSEC, starttime)
+                while True:
+                    success,image = vidcap.read()
+                    currentime= vidcap.get(cv2.CAP_PROP_POS_MSEC)
+                    if(currentime> endtime):
+                        break
+                    if success:
+                        filename='image'+ str(i)+ '.jpg'
+                        cv2.imwrite(filename, image)
+                    else:
+                        break
