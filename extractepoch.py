9f9f9c585bf2b4e9da999cbdc868f6a83ce04498
sujayr91 <sujayr91@gmail.com>
2017-08-05 00:53:58 -0400

    fixclipping

--- extractepoch.py
+++ extractepoch.py
@@ -1,26 +1,32 @@
 import os
 import cv2
-
+from pdb import set_trace as st
 folders=['drowsy','distracted','attentive']
 inputpath='./rawdata'
-outputpath='./clippeddata'
+outputpath='./clipped'
 
 for folder in folders:
 	videopath=os.path.join(inputpath, folder)
 	tripvideos= os.listdir(videopath)
 	for tripvideo in tripvideos:
+		tripimagesfolder=os.path.splitext(tripvideo)
+		outpath= os.path.join(outputpath,folder, tripimagesfolder[0])
+		if not os.path.exists(outpath):
+			os.makedirs(outpath)
 		video= os.path.join(videopath, tripvideo)
-                vidcap = cv2.VideoCapture(video)
-                starttime=20000
-                endtime=40000
-                vidcap.set(cv2.CAP_PROP_POS_MSEC, starttime)
-                while True:
-                    success,image = vidcap.read()
-                    currentime= vidcap.get(cv2.CAP_PROP_POS_MSEC)
-                    if(currentime> endtime):
-                        break
-                    if success:
-                        filename='image'+ str(i)+ '.jpg'
-                        cv2.imwrite(filename, image)
-                    else:
-                        break
+		vidcap = cv2.VideoCapture(video)
+		starttime=20000
+		endtime=40000
+		vidcap.set(cv2.CAP_PROP_POS_MSEC, starttime)
+		i=1
+		while True:
+			success,image = vidcap.read()
+			currentime= vidcap.get(cv2.CAP_PROP_POS_MSEC)
+			if(currentime> endtime):
+				break
+			if success:
+				filename='image'+ str(i)+ '.jpg'				
+				cv2.imwrite(os.path.join(outpath,filename), image)
+				i+=1
+			else:
+				break
